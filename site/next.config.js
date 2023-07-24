44d831b1862cdf7085f729349a3376f186627e68
kujeezy <91900421+kujeezy@users.noreply.github.com>
2023-07-24 10:40:16 -0500

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Kujo Kasm Registry',
+    description: 'Kujo Kasm Workspaces Registry',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://github.com/kujeezy/kujo-kasm-registry/',
+    contactUrl: 'https://github.com/kujeezy/kujo-kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kujo-kasm-registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
